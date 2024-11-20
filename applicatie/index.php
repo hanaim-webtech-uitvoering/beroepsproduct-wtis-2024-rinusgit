c7921763ee1f8bfc414e42b8e874a6c585c29074
rinusgit <mht.reijers@student.han.nl>
2024-11-20 10:49:42 +0100

    Update index.php
    Test

--- applicatie/index.php
+++ applicatie/index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title> Pizzeria Sole Machina</title>
 </head>
 <body>
-    <h1>It Works!</h1>
+    <h1>!</h1>
     <?php echo('Hallo WT\'er, de webserver is online en PHP werkt.'); ?>
     <br>
     <br>
